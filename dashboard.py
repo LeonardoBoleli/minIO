10f87bef6f0d35fe8a50fc5721b1e3bb64045890
Leonardo Boleli <56308143+LeonardoBoleli@users.noreply.github.com>
2023-06-02 09:04:03 -0300

    asd

--- dashboard.py
+++ dashboard.py
@@ -23,7 +23,7 @@ data['valor'] = data['valor'].str.replace('.', '').str.replace(',', '.').astype(
 # Valor médio dos produtos por site
 valor_medio_por_site = data.groupby('site')['valor'].mean()
 
-# Filtra os dados para o link do processador
+# Filtra os dados para os links dos componentes
 link_processador = "https://produto.mercadolivre.com.br/MLB-2644395073-processador-intel-core-i7-10700-box-lga-1200-bx8070110700-_JM#position=11&search_layout=grid&type=item&tracking_id=a1976802-4bbe-4d4d-a00b-dfbda8b60ce9"
 link_gabinete = "https://www.mercadolivre.com.br/gabinete-gamer-lian-li-redragon-modelo-o11dynamic-mini-branc/p/MLB23190291?pdp_filters=category:MLB1696#searchVariation=MLB23190291&position=2&search_layout=grid&type=product&tracking_id=dab59008-df8b-46f0-bd34-2f8053eca38f"
 link_placa_de_video = "https://www.mercadolivre.com.br/placa-de-video-nvidia-galax-geforce-rtx-30-series-rtx-3060-36nsl8md6occ-oc-edition-8gb/p/MLB20736337?pdp_filters=category:MLB1658#searchVariation=MLB20736337&position=3&search_layout=grid&type=product&tracking_id=d3ba3a55-4cda-4a8e-8f88-fd6382009246"
@@ -31,57 +31,46 @@ link_placa_mae = "https://produto.mercadolivre.com.br/MLB-1676543787-placa-me-as
 link_memoria_ram = "https://www.mercadolivre.com.br/memoria-ram-fury-color-preto-16gb-1-hyperx-hx426c16fb16/p/MLB14728888?pdp_filters=category:MLB1694#searchVariation=MLB14728888&position=8&search_layout=grid&type=product&tracking_id=b0b0bebf-c99d-42c0-b721-f62d1a64d3a1"
 link_water_cooler = "https://produto.mercadolivre.com.br/MLB-3381940936-water-cooler-corsair-h100-rgb-240mm-radiator-preto-_JM#position=6&search_layout=grid&type=item&tracking_id=137704c3-2bb1-4abe-8809-bd43e8c8f05d"
 
-processador = data.loc[data['link'] == link_processador, 'valor'].tolist()
-gabinete = data.loc[data['link'] == link_gabinete, 'valor'].tolist()
-placa_de_video = data.loc[data['link'] == link_placa_de_video, 'valor'].tolist()
-placa_mae = data.loc[data['link'] == link_placa_mae, 'valor'].tolist()
-memoria_ram = data.loc[data['link'] == link_memoria_ram, 'valor'].tolist()
-water_cooler = data.loc[data['link'] == link_water_cooler, 'valor'].tolist()
-
-# Imprime as informações no terminal
-print("Dashboard")
-print("---------")
-componentes = {
-    "Processador": processador,
-    "Gabinete": gabinete,
-    "Placa de Vídeo": placa_de_video,
-    "Placa Mãe": placa_mae,
-    "Memória RAM": memoria_ram,
-    "Water Cooler": water_cooler
-}
-
 # Dicionário para armazenar os valores de cada componente
 valores_componentes = {}
-
-for componente, link in componentes.items():
-    valores = data.loc[data['link'] == link, 'valor'].tolist()
-    valores = [valor.replace('.', '').replace(',', '.') for valor in valores]
-    valores = [float(valor) for valor in valores]
-    valores_componentes[componente] = valores
-
-    print(f"Valores do {componente}:")
-    print(valores)
-    print()
 
 # Verifica se o link do processador está presente no DataFrame
 if link_processador in data['link'].tolist():
     processador = data.loc[data['link'] == link_processador, 'valor'].tolist()
-
-    print("Valores do processador:")
-    print(processador)
-    print()
+    valores_componentes["Processador"] = processador
 else:
     print("Não há valores para o processador")
     print()
 
-# Imprime as informações de cada componente
+# Filtra os valores dos outros componentes
+for componente, link in {
+    "Gabinete": link_gabinete,
+    "Placa de Vídeo": link_placa_de_video,
+    "Placa Mãe": link_placa_mae,
+    "Memória RAM": link_memoria_ram,
+    "Water Cooler": link_water_cooler
+}.items():
+    if link in data['link'].tolist():
+        valores = data.loc[data['link'] == link, 'valor'].tolist()
+        valores_componentes[componente] = valores
+    else:
+        print(f"Não há valores para o {componente}")
+        print()
+
+# Imprime as informações no terminal
+print("Dashboard")
+print("---------")
+
 for componente, valores in valores_componentes.items():
+    print(f"Valores do {componente}:")
+    print(valores)
+    print()
+
     if len(valores) > 0:
         max_valor = np.max(valores)
         min_valor = np.min(valores)
         media_valor = np.mean(valores)
         
-        print(f"Valores do {componente}:")
         print("Máximo:", max_valor)
         print("Mínimo:", min_valor)
         print("Média:", media_valor)
