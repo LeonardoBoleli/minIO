8343d4139b0b2e9597174fff9f0563524b570efd
Leonardo Boleli <56308143+LeonardoBoleli@users.noreply.github.com>
2023-06-04 14:15:18 -0300

    asd

--- datawarehouse.py
+++ datawarehouse.py
@@ -51,7 +51,7 @@ with conn.cursor() as cursor:
             INSERT INTO warehouse (produto, valor, link, data, hora, min_valor, avg_valor, max_valor)
             VALUES (%s, %s, %s, %s, %s, %s, %s, %s);
         """
-        cursor.execute(insert_query, *row)
+        cursor.execute(insert_query, row)
 conn.commit()
 print("Inserção concluída com sucesso!")
 
